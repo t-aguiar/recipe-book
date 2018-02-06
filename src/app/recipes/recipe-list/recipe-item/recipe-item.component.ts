714a4c49a78c44b82cd68308060d93ebb67dfb97
t-aguiar <antoner.aguiar@gmail.com>
2018-02-05 22:24:03 -0800

    Implementing recipe components

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
+import {Recipe} from "../../recipe.model";
 
 @Component({
   selector: 'app-recipe-item',
@@ -6,8 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent implements OnInit {
+  @Input() recipe: Recipe;
 
-  constructor() { }
+  constructor(recipe: Recipe) {
+    this.recipe = recipe;
+  }
 
   ngOnInit() {
   }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+      new Recipe('Taquitos', 'Yummy goodness', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShVpLC1CxENS9BTF_zaV7bML21TGM6GvljpSIGLHttaf_eEJQC')
+  ];
 
   constructor() { }
 
